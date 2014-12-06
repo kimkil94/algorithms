3e1471212c71854fc3a8ae9e9ad08ba85a564379
Kimkil <nebster94@gmail.com>
2014-12-06 04:28:28 +0100

    added merge-sort

--- sorts/sorts.c
+++ sorts/sorts.c
@@ -59,39 +59,77 @@ void bubble(int p[],int n){
 	int i,j;
 	int x;
     for(i=1;i<n;i++){
+     printf("\n");
+//     for(int c=0;c<n;c++){ printf("%d ",p[c]); }
      for(j=n-1;j>=i;j--){
+        //printf("%d",p[5]);
         if(p[j-1] > p[j]){
+
 		x=p[j-1];
 		p[j-1]=p[j];
 		p[j]=x;
+            
 
+
+          printf("Xje: %d\n",x);
+	  for(int c=0;c<n;c++){ printf("%d ",p[c]); }
 
 	   }
+	
 
-	}
+}
 
 
+printf("%d",x);
 
 
     }
 for(int c=0;c<n;c++){ printf("%d ",p[c]); } 
 }
 
+void merge(int p[],int q[],int l,int r){
+int i,s,j,k;
+s=(l+r)/2;
+if(l<s) merge(p,q,l,s);
+if(s+1<r) merge(p,q,s+1,r);
+/*zlucovanie usekov*/
+i=l; j=s+1;k=l;
+while((i<=s)&&(j<=r)){
+  if(p[i]<=p[j])
+	q[k]=p[i++];
+  else
+	q[k]=p[j++];
+  k++;
+}
+while(i<=s){
+  q[k]=p[i++];k++;
+}
+while(j<=r){
+q[k]=p[j++]; k++;
+}
+/*Preniesieme zotriedeny ysek spat z q do p*/
+printf("\n");
+for(k=1;k<=r;k++){
+    p[k]=q[k];
 
+printf("%d ",p[k]);}
+
+}
 
 int main(){
 
 int n=10;
-int p[9]={1,5,6,8,3,11,13,19,0};
-printf("SELECT-SORT:\n ");
-select(p,n);
+int p[9]={12,15,62,83,34,11,13,19,8};
+int f[9];
+//printf("SELECT-SORT:\n ");
+//select(p,n);
+//printf("\n");
+//printf("INSERT-SORT\n");
+//InsSort(p,n);
+//printf("\n");
+//printf("BUBBLE-SORT\n");
+//bubble(p,n);
 printf("\n");
-printf("INSERT-SORT\n");
-InsSort(p,n);
-printf("\n");
-printf("BUBBLE-SORT\n");
-bubble(p,n);
-printf("\n");
-
+merge(p,f,1,9);
 
 }
